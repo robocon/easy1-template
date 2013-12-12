1288a1987e92e7ebf61c80c1aaad18c8b92b2888
Kritsanasak <roboconk@gmail.com>
2013-12-12 13:39:36 +0700

    - เพิ่มรายละเอียดลงใน readme - ลบ license ตัวเดิมที่เป็น creativecommons

--- index.php
+++ index.php
@@ -131,9 +131,6 @@ $template = $db->fetch($query_template);
                     <p><?php echo $config['name']?></p>
                     <?php } ?>
                     <p>HTML5 Template from <a href="http://www.initializr.com/">Initializr</a>, redesign by <a href="mailto:kinglizardsg@gmail.com">KinglizardSG</a></p>
-                    <p>
-                        <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
-                    </p>
                 </div>
             </footer>
         </div>
